8d7004e174ee9fa6fd2d63a6c15feba862ac3bd8
Jimmy Chu <jimmychu0807@gmail.com>
2018-11-19 11:33:40 +0800

    completed test

--- truffle.js
+++ truffle.js
@@ -1,5 +1,10 @@
 module.exports = {
   networks: {
+    ganache_gui: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
     development: {
       host: "localhost",
       port: 8545,
